78dcc3eef2b92f26468f3f58e4d4e5a1208ffc7d
pratik-jaysingpure <pratikjaysingpure9@gmail.com>
2025-08-13 17:47:55 +0530

    add filght

---
+++ src/main/java/com/example/yatra/Flight.java
@@ -0,0 +1,13 @@
+package com.example.yatra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Flight {
+
+    @GetMapping("/flight")
+    public String flight() {
+        return "Welcome to the My Flight";
+    }
+}
