494df8017ac078d16df7b78ff3d385f2ba592056
pratik-jaysingpure <pratikjaysingpure9@gmail.com>
2025-09-16 16:30:39 +0530

    update jenkins

--- src/main/java/com/example/yatra/Holiday.java
+++ src/main/java/com/example/yatra/Holiday.java
@@ -8,6 +8,6 @@ public class Holiday {
 
     @GetMapping("/holiday")
     public String holiday() {
-        return "using mvn clean package this is fast build check Holiday fetal using yatra ms ";
+        return "using mvn clean package this is fast build check Holiday  using yatra ms ";
     }
 }
