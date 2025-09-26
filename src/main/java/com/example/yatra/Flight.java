7aca3729b31d66d4623a66850f8a0e0c1c2a62da
Pratik Jaysingpure <pratikjaysingpure9@gmail.com>
2025-09-26 14:20:45 +0530

    update dev

--- src/main/java/com/example/yatra/Flight.java
+++ src/main/java/com/example/yatra/Flight.java
@@ -8,6 +8,6 @@ public class Flight {
 
     @GetMapping("/flight")
     public String flight() {
-        return "Welcome to the My Flight";
+        return " pratik Welcome to the My Flight";
     }
 }
