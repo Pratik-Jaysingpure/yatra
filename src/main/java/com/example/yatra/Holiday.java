00b10b5123028e82af8594991f1c1cfdaa992af1
Pratik Jaysingpure <pratikjaysingpure9@gmail.com>
2025-09-27 19:12:46 +0530

    Webhook test

--- src/main/java/com/example/yatra/Holiday.java
+++ src/main/java/com/example/yatra/Holiday.java
@@ -8,6 +8,6 @@ public class Holiday {
 
     @GetMapping("/holiday")
     public String holiday() {
-        return "using mvn clean package this is ds is build check pratik add the pool scm  ";
+        return "using mvn clean package this is ds is build check  jay pratik add the pool scm  ";
     }
 }
