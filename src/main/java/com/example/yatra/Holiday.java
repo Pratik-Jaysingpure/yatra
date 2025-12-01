55d40cef84591f410a9f8ca45e139ca77a704e50
Pratik Jaysingpure <97844878+Pratik-Jaysingpure@users.noreply.github.com>
2025-12-01 18:56:32 +0530

    Update holiday message in Holiday.java
    add

--- src/main/java/com/example/yatra/Holiday.java
+++ src/main/java/com/example/yatra/Holiday.java
@@ -8,6 +8,6 @@ public class Holiday {
 
     @GetMapping("/holiday")
     public String holiday() {
-        return "hi using mvn  package Sumit  " ;
+        return "hi using mvn  package fsdfdsfdsfsdfasdfasfa  " ;
     }
 }
