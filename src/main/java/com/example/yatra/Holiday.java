143d69d58aab5aee834d8f91dddfa98fe2f1f0c5
Pratik Jaysingpure <pratikjaysingpure9@gmail.com>
2025-12-01 19:17:08 +0530

    pratik

--- src/main/java/com/example/yatra/Holiday.java
+++ src/main/java/com/example/yatra/Holiday.java
@@ -8,6 +8,6 @@ public class Holiday {
 
     @GetMapping("/holiday")
     public String holiday() {
-        return "hi using mvn  package fsdfdsfdsfsdfasdfasfa  pratik + sumit " ;
+        return "pratik " ;
     }
 }
