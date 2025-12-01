916a81c5551340f7f097e80f9e63c242bf019314
Pratik Jaysingpure <pratikjaysingpure9@gmail.com>
2025-12-01 19:49:09 +0530

    sumit added

--- src/main/java/com/example/yatra/Holiday.java
+++ src/main/java/com/example/yatra/Holiday.java
@@ -8,6 +8,6 @@ public class Holiday {
 
     @GetMapping("/holiday")
     public String holiday() {
-        return "sumit fdsfsdfdsfdsfdfsdff" ;
+        return "sumit fdsfsdfdsfdsfdfsdfffsdfdfdfdf  " ;
     }
 }
